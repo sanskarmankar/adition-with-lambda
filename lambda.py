ea4c683fb51a92a589b17ed69eec0c831fc3bfa7
sanskarmankar <82326357+sanskarmankar@users.noreply.github.com>
2023-02-27 03:18:38 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,10 @@
+length=int(input("Enter length : "))
+breadth=int(input("enter breadth:"))
+
+if length != 0 and breadth != 0:s
+    if length==breadth:
+         print("it is a square")
+    else:``
+         print("its a rectangle")    
+else:
+     print("enter a non zero no")         
